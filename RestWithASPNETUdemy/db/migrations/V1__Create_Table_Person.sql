b2b0cb39f98abec973957f28645ea6ae2e322500
Carlos Eduardo <carlos.eduardo@ceresinvestimentos.com>
2021-06-23 13:49:47 -0300

    WIP: Inserção de dados na tabela de pessoas

--- RestWithASPNETUdemy/db/migrations/V1__Create_Table_Person.sql
+++ RestWithASPNETUdemy/db/migrations/V1__Create_Table_Person.sql
@@ -3,5 +3,5 @@ CREATE TABLE if not exists public.person (
 	first_name VARCHAR ( 80 ) NOT NULL,
 	last_name VARCHAR ( 80 ) NOT NULL,
 	address VARCHAR ( 100 ) NOT NULL,
-	gender VARCHAR(6) NOT NULL 
+	gender VARCHAR(20) NOT NULL 
 );
